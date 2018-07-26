3a00c511cc6371bd24dc4c311b173ddad6c3ed56
eorbach <evyataro@gmail.com>
2018-07-26 12:28:00 +0300

    more elegant solution

--- exercise1.py
+++ exercise1.py
@@ -6,8 +6,8 @@ Create a program that asks the user to enter their name and their age. Print out
 '''
 
 from datetime import datetime
-
 year = datetime.now().strftime("%Y")
 name = raw_input("Please enter your name\n")
 age = raw_input("Please enter your age\n")
-print("Hi " + name + ",\nIn 100 year from now you will be " + str(int(age) + 100) + " and the year is going to be " + str(int(year) + 100))
+print("Hi %s,\nIn 100 year from now you will be %s and the year is going to be %s" %(name, str(int(age) + 100), str(int(year) + 100)))
+
