fd7c5d708e69b52d2343cb7525548645225ac1a9
eorbach <evyataro@gmail.com>
2018-07-26 12:12:23 +0300

    add first python exercise

---
+++ exercise1.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+__author__ = "Evyatar Orbach"
+__email__ = "evyataro@gmail.com"
+'''Exercise 1
+Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old.
+'''
+
+from datetime import datetime
+
+year = datetime.now().strftime("%Y")
+name = raw_input("Please enter your name\n")
+age = raw_input("Please enter your age\n")
+print("Hi " + name + ",\nIn 100 year from now you will be " + str(int(age) + 100) + " and the year is going to be " + str(int(year) + 100))
